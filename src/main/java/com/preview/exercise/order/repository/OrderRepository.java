610b5967574cfb880e7171e3c8afcbb8d9659a32
HyeonBin2379 <hyeonBin2379@gmail.com>
2026-01-30 16:54:53 +0900

    refactor: ãRepository 어노테이션 수정

--- src/main/java/com/preview/exercise/order/repository/OrderRepository.java
+++ src/main/java/com/preview/exercise/order/repository/OrderRepository.java
@@ -2,8 +2,6 @@ package com.preview.exercise.order.repository;
 
 import com.preview.exercise.order.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- src/main/java/com/preview/exercise/product/repository/ProductRepository.java
+++ src/main/java/com/preview/exercise/product/repository/ProductRepository.java
@@ -5,9 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     @Query("select p from Product p where p.id = :productId and p.isExisted = true")
